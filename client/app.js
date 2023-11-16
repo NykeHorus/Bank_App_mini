2c084e5c68d1c89891348d90a0f4ba5793cecc66
Hariss <harissbilal@hotmail.com>
2023-11-16 11:01:01 +0100

    Added comments

--- client/app.js
+++ client/app.js
@@ -1,16 +1,28 @@
-let account = null;
+//-------------------------------------------------------------
+//Constants
+//-------------------------------------------------------------
+const storageKey = "savedAccount";
+
+//-------------------------------------------------------------
+// Router
+//-------------------------------------------------------------
 
 const routes = {
-  "/login": { templateId: "login" },
-  "/dashboard": { templateId: "dashboard", init: updateDashboard },
+  "/login": { title: "My Account", templateId: "login" },
+  "/dashboard": { title: "Login", templateId: "dashboard", init: refresh },
 };
+
+function navigate(path) {
+  window.history.pushState({}, path, path);
+  updateRoute();
+}
 
 function updateRoute() {
   const path = window.location.pathname;
   const route = routes[path];
 
   if (!route) {
-    return navigate("/login");
+    return navigate("/dashboard");
   }
 
   const template = document.getElementById(route.templateId);
@@ -24,30 +36,19 @@ function updateRoute() {
   }
 }
 
-function navigate(path) {
-  window.history.pushState({}, path, path);
-  updateRoute();
-}
+//-------------------------------------------------------------
+//API Interactions
+//-------------------------------------------------------------
 
-function onLinkClick(event) {
-  event.preventDefault();
-  navigate(event.target.href);
-}
-
-async function register() {
-  const registerForm = document.getElementById("registerForm");
-  const formData = new FormData(registerForm);
-  const data = Object.fromEntries(formData);
-  const jsonData = JSON.stringify(data);
-  const result = await createAccount(jsonData);
-
-  if (result.error) {
-    return updateElement("registerError", result.error);
+async function getAccount(user) {
+  try {
+    const response = await fetch(
+      "//localhost:5000/api/accounts/" + encodeURIComponent(user)
+    );
+    return await response.json();
+  } catch (error) {
+    return { error: error.message || "Unknown error" };
   }
-  console.log("Account created: ", result);
-
-  account = result;
-  navigate("/dashboard");
 }
 
 async function createAccount(account) {
@@ -63,6 +64,37 @@ async function createAccount(account) {
   }
 }
 
+function createTransactionRow(transaction) {
+  const template = document.getElementById("transaction");
+  const transactionRow = template.content.cloneNode(true);
+  const tr = transactionRow.querySelector("tr");
+  tr.children[0].textContent = transaction.date;
+  tr.children[1].textContent = transaction.object;
+  tr.children[2].textContent = transaction.amount.toFixed(2);
+  return transaction;
+}
+
+//-------------------------------------------------------------
+//Global States
+//-------------------------------------------------------------
+
+let state = Object.freeze({
+  account: null,
+});
+
+function updateState(property, newData) {
+  state = Object.freeze({
+    ...state,
+    [property]: newData,
+  });
+  localStorage.setItem(storageKey, JSON.stringify(state.account));
+  console.log(state);
+}
+
+//-------------------------------------------------------------
+//login/register
+//-------------------------------------------------------------
+
 async function login() {
   const loginForm = document.getElementById("loginForm");
   const user = loginForm.user.value;
@@ -71,42 +103,66 @@ async function login() {
   if (data.error) {
     return updateElement("loginError", data.error);
   }
-  account = data;
+  updateState("account", data);
   navigate("/dashboard");
 }
 
-async function getAccount(user) {
-  try {
-    const response = await fetch(
-      "//localhost:5000/api/accounts/" + encodeURIComponent(user)
-    );
-    return await response.json();
-  } catch (error) {
-    return { error: error.message || "Unknown error" };
+async function register() {
+  const registerForm = document.getElementById("registerForm");
+  const formData = new FormData(registerForm);
+  const data = Object.fromEntries(formData);
+  const jsonData = JSON.stringify(data);
+  const result = await createAccount(jsonData);
+
+  if (result.error) {
+    return updateElement("registerError", result.error);
   }
+  console.log("Account created: ", result);
+
+  updateState("account", result);
+  navigate("/dashboard");
 }
 
-function updateElement(id, textOrNode) {
-  const element = document.getElementById(id);
-  element.textContent = "";
-  element.append(textOrNode);
+//-------------------------------------------------------------
+//Dashboard
+//-------------------------------------------------------------
+
+async function updateAccountData() {
+  const account = state.account;
+  if (!account) {
+    return logout();
+  }
+
+  const data = await getAccount(account.user);
+  if (data.error) {
+    return logout();
+  }
+
+  updateState("account", data);
+}
+
+async function refresh() {
+  await updateAccountData();
+  updateDashboard();
 }
 
 function updateDashboard() {
+  const account = state.account;
   if (!account) {
-    return navigate("/login");
-  }
-
-  const transactionsRows = document.createDocumentFragment();
-  for (const transaction of account.transactions) {
-    const transactionRow = createTransactionRow(transaction);
-    transactionsRows.appendChild(transactionRow);
+    return logout();
   }
 
   updateElement("description", account.description);
   updateElement("balance", account.balance.toFixed(2));
   updateElement("currency", account.currency);
   updateElement("transactions", transactionsRows);
+
+  // Update transactions
+  const transactionsRows = document.createDocumentFragment();
+  for (const transaction of account.transactions) {
+    const transactionRow = createTransactionRow(transaction);
+    transactionsRows.appendChild(transactionRow);
+  }
 }
 
 function createTransactionRow(transaction) {
@@ -119,5 +175,40 @@ function createTransactionRow(transaction) {
   return transaction;
 }
 
-window.onpopstate = () => updateRoute();
-updateRoute();
+function logout() {
+  updateState("account", null);
+  navigate("/login");
+}
+
+//-------------------------------------------------------------
+//Utils
+//-------------------------------------------------------------
+
+function updateElement(id, textOrNode) {
+  const element = document.getElementById(id);
+  element.textContent = ""; //Removes all children
+  element.append(textOrNode);
+}
+
+function onLinkClick(event) {
+  event.preventDefault();
+  navigate(event.target.href);
+}
+
+//-------------------------------------------------------------
+//Init
+//-------------------------------------------------------------
+
+function init() {
+  //Restore State
+  const savedAccount = localStorage.getItem(storageKey);
+  if (savedAccount) {
+    updateState("account", JSON.parse(savedAccount));
+  }
+
+  //Update route for browser back/next buttons
+  window.onpopstate = () => updateRoute();
+  updateRoute();
+}
+
+init();
